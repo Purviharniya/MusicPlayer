c3edbb199b6e178689eae94e005eff514784bb8b
unknown <purvi.harniya@gmail.com>
2021-06-22 15:37:06 +0530

    Updates

---
+++ precache-manifest.d3070b99d9bcd341c57cdb27ff0aad2c.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4548e9050f33d017ccff91808413517b",
+    "url": "/MusicPlayer/index.html"
+  },
+  {
+    "revision": "d3209f4b09dfbf16b86d",
+    "url": "/MusicPlayer/static/css/main.23c028c2.chunk.css"
+  },
+  {
+    "revision": "f2f8b3a49c9a70f489ab",
+    "url": "/MusicPlayer/static/js/2.af5104e3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/MusicPlayer/static/js/2.af5104e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3209f4b09dfbf16b86d",
+    "url": "/MusicPlayer/static/js/main.1918a7a2.chunk.js"
+  },
+  {
+    "revision": "b04f916382d8a3cc6596",
+    "url": "/MusicPlayer/static/js/runtime-main.d1814e7e.js"
+  },
+  {
+    "revision": "fbcc26cac01167dd51983660005e63df",
+    "url": "/MusicPlayer/static/media/All I Want.fbcc26ca.mp3"
+  },
+  {
+    "revision": "37f6431fdc76f7368a78ecca7c7bb7af",
+    "url": "/MusicPlayer/static/media/All of Me.37f6431f.mp3"
+  },
+  {
+    "revision": "af80e13f297a76f85eee6d50b33c0632",
+    "url": "/MusicPlayer/static/media/Attention.af80e13f.mp3"
+  },
+  {
+    "revision": "233d2cf1e0d1504fc2eeae11b48f39d9",
+    "url": "/MusicPlayer/static/media/Believer.233d2cf1.mp3"
+  },
+  {
+    "revision": "ff5c9b9895b91c3aad7573378ef06ddd",
+    "url": "/MusicPlayer/static/media/Better Now.ff5c9b98.mp3"
+  },
+  {
+    "revision": "119d24961ef9c7ca05a66586e8b8bd2b",
+    "url": "/MusicPlayer/static/media/Dangerously.119d2496.mp3"
+  },
+  {
+    "revision": "d75e9fe9cabebd0516285d89f9f762cc",
+    "url": "/MusicPlayer/static/media/Happier.d75e9fe9.mp3"
+  },
+  {
+    "revision": "9c7dfb1034ed6813cfcc3ff1ffbfa117",
+    "url": "/MusicPlayer/static/media/It Ain't Me.9c7dfb10.mp3"
+  },
+  {
+    "revision": "24c75ff9f40db477ce7c08a6619b13a4",
+    "url": "/MusicPlayer/static/media/Just The Way You Are.24c75ff9.mp3"
+  },
+  {
+    "revision": "814f68e302f7447fe8664b01a7bd61d1",
+    "url": "/MusicPlayer/static/media/Lucid Dreams.814f68e3.mp3"
+  },
+  {
+    "revision": "edfede39ce6a022f871ad49ad0b00f02",
+    "url": "/MusicPlayer/static/media/Make You Mine.edfede39.mp3"
+  },
+  {
+    "revision": "9c2f862a94638dd637b3bacd89639f16",
+    "url": "/MusicPlayer/static/media/Talking to the Moon.9c2f862a.mp3"
+  },
+  {
+    "revision": "699f4d4c08aca0325949042e435120ce",
+    "url": "/MusicPlayer/static/media/There For You.699f4d4c.mp3"
+  },
+  {
+    "revision": "a9765477562f71abb56a69fd3fdc0bdb",
+    "url": "/MusicPlayer/static/media/Without Me.a9765477.mp3"
+  },
+  {
+    "revision": "e97b0a72306926087631dd15a7c4c323",
+    "url": "/MusicPlayer/static/media/You Are The Reason.e97b0a72.mp3"
+  },
+  {
+    "revision": "97077d69468287396491ef66d338c79c",
+    "url": "/MusicPlayer/static/media/album1.97077d69.jpg"
+  },
+  {
+    "revision": "6a6832821eb1bf4df7268097e24e3de9",
+    "url": "/MusicPlayer/static/media/album11.6a683282.jpg"
+  },
+  {
+    "revision": "b3f490dd6db45f2e74df33f916fcc1dd",
+    "url": "/MusicPlayer/static/media/album2.b3f490dd.jpg"
+  },
+  {
+    "revision": "1fae235400589befc0fb15c7171cf409",
+    "url": "/MusicPlayer/static/media/album9.1fae2354.jpg"
+  },
+  {
+    "revision": "2c0fe3b1f1aeb5e684a7b05b68716c6c",
+    "url": "/MusicPlayer/static/media/artist8.2c0fe3b1.jpg"
+  },
+  {
+    "revision": "f6db2dd9e12f32f819e053cc3d93d354",
+    "url": "/MusicPlayer/static/media/just the way you are.f6db2dd9.jpg"
+  },
+  {
+    "revision": "df133cc4df36835522b731457a240775",
+    "url": "/MusicPlayer/static/media/logo.df133cc4.png"
+  },
+  {
+    "revision": "9d08c1cb910b9ac69e9aae9fee3b0818",
+    "url": "/MusicPlayer/static/media/make you mine.9d08c1cb.jpg"
+  }
+]);
